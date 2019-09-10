8c85b17a546696828f6de2fd8737ea38eadd260a
Hans-Jakob <hans-jakob.brandt@MWNOOSLMACLT021.local>
2019-09-10 13:44:56 +0200

    Change custom rules from error to warning

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
   rules: {
     // 'object-curly-spacing': ['error', 'never'],
-    'jsx-quotes': ['error', 'prefer-double'],
-    'space-before-function-paren': ['error', 'never']
+    'jsx-quotes': ['warning', 'prefer-double'],
+    'space-before-function-paren': ['warning', 'never']
   },
   settings: {
     react: {
