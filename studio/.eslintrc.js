124a0ac47f1af6fec01fe7f4cbf18c2751b2871c
Nicolas Tonne <nicolas.tonne@makingwaves.com>
2019-09-11 16:33:52 +0200

    Eslint fix

--- studio/.eslintrc.js
+++ studio/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['standard', 'standard-react'],
   parser: 'babel-eslint',
   rules: {
-    'react/prop-types': 0
+    'react/prop-types': 0,
     // 'object-curly-spacing': ['error', 'never']
     'jsx-quotes': ['error', 'prefer-double'],
     'space-before-function-paren': ["error", "never"]
